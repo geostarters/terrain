06ef801a083454e0a8ad9e82c6484c18fd0373b3
Isaac Besora Vilardaga <isaac@altersport.cat>
2017-12-01 10:06:50 +0100

    Lerps new points if the track doesn't have a minimum

--- gdal_location_info.js
+++ gdal_location_info.js
@@ -3,6 +3,8 @@
 var gdal = require('gdal');
 const request = require("request-promise");
 
+var minPointsInTrack = 100;
+
 var elevation = function(file) {
     try {
         var raster = gdal.open(file);
@@ -44,11 +46,66 @@ var elevation = function(file) {
             return [point[0], point[1], await elevate.z(point[0], point[1])];
         },
         linestring: async function(linestring) {
+
+            var newLineString = elevate.addPointsIfNotMinimumNumber(linestring);
             var count = linestring.length;
+
             for (var i = 0; i < count; i++) {
-                linestring[i] = await elevate.point(linestring[i]);
+                newLineString[i] = await elevate.point(newLineString[i]);
             }
-            return linestring;
+
+            return newLineString;
+        },
+        addPointsIfNotMinimumNumber: function(linestring) {
+
+            var newLineString = linestring;
+            var count = linestring.length;
+
+            if(count < minPointsInTrack)
+                newLineString = elevate.addPointsIntoLineString(linestring);
+
+            return newLineString;
+
+        },
+        addPointsIntoLineString: function(linestring) {
+
+            var newLineString = linestring.slice();
+            var count = linestring.length;
+            var pointsToGenerate = minPointsInTrack - count;
+            var trackSegments = count - 1;
+            var pointsInSegment = Math.ceil(pointsToGenerate / trackSegments) + 1;
+            var insertionPoint = 0;
+
+            console.log(count + " input points");
+            console.log(trackSegments + " track segments");
+            console.log(pointsToGenerate + " points to generate");
+            console.log(pointsInSegment + " points in each segment");
+
+            for(var i=0; i<count-1; ++i) {
+
+                var startPoint = linestring[i];
+                var endPoint = linestring[i+1];
+                var xInc = endPoint[0] - startPoint[0];
+                var yInc = endPoint[1] - startPoint[1];
+                var xIncPerPoint = xInc / pointsInSegment;
+                var yIncPerPoint = yInc / pointsInSegment;
+
+                for(var j=1; j<pointsInSegment; ++j) {
+                 
+                    var newPoint = [startPoint[0] + xIncPerPoint*j, 
+                        startPoint[1] + yIncPerPoint*j];
+                    newLineString.splice(++insertionPoint, 0, newPoint);
+
+                    console.log("Adding point " + newPoint[0] + "," + newPoint[1] + " between " + i + " and " + (i+1) + " at " + insertionPoint);
+                    
+                }
+
+                ++insertionPoint;
+
+            }
+
+            return newLineString;
+
         },
         polygon: async function(polygon) {
             var count = polygon.length;
